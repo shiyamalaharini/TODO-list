cfa1022e3bb78240a92a7b66d2acf764aa3f13c5
shiyamalaharini <harinishiyam@gmail.com>
2025-01-19 20:18:38 +0530

    change code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    outDir: 'dist', // Ensure this is 'dist' or matches your deployment
+  },
+};
